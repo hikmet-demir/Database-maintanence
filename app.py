07e3aa00edd8043f3c920bdccabfd74bf6899e42
Hikmet <hikmet5548@gmail.com>
2019-12-25 22:03:24 +0300

    templates added

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__,static_url_path="/static/")
 @app.route('/')
 
 def hello():
-    return "SIMKIIII HİKOOO MUROO AHMOO"
-
+    return render_template('index.html', index=1)
 
 if __name__ =="__main__":
     app.run(host="localhost",port="8000",debug=True)
