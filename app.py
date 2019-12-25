e8ab30fb91d147f754861b137fca01152769927b
Hikmet <hikmet5548@gmail.com>
2019-12-25 21:41:25 +0300

    website up and running

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask, request, render_template
+
+app = Flask(__name__,static_url_path="/static/")
+
+@app.route('/')
+
+def hello():
+    return "SIMKIIII HİKOOO MUROO AHMOO"
+
+
+if __name__ =="__main__":
+    app.run(host="localhost",port="8000",debug=True)
