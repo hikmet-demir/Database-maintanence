45bde8d9b7b2094cb7369cece9cc44a4f9ba7051
Ahmet Avcı <ahmetavci@Ahmet-MacBook-Pro.local>
2019-12-26 19:33:55 +0300

    git ignore

---
+++ flaskr/customer_service_asisstant.py
@@ -0,0 +1,10 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('customer_service_asisstant', __name__, url_prefix='/customer_service_asisstant')
--- flaskr/technician.py
+++ flaskr/technician.py
@@ -3,7 +3,6 @@ import functools
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
