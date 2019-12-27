f7ad819f1e7e57e551063965b584434b1e4e3646
Simge <simge.tbk@gmail.com>
2019-12-27 18:54:08 +0300

    admin page

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -8,3 +8,16 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
+@bp.route('/getCustomers',methods =('GET','POST'))
+def getCustomers():
+    db = get_db()
+
+    customers =  db.execute(
+            'SELECT * FROM customer')
+        ).fetchall()
+
+    # outer brackets are required, it doesnt work without them somehow
+    # thats why id's are shown in the bracets on the website
+    data = [[i[0] for i in products]]
+
+    return render_template('admin/admin.html', data = data, size = len(data))
